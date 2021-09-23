5baf610dc929a418842928e614e6d4ad6250f724
kouhei-focus <mpz7kouhei@gmail.com>
2021-09-23 09:34:51 +0900

    課題の機能を追加した

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -6,16 +6,48 @@
 //
 
 import UIKit
+import RealmSwift
+//ユーザーのデータを保存するためにrealmをimportする
+
+import UserNotifications
+import DeveloperToolsSupport
+//userの通知を送るためにusernotificationsをimportする
+
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-  
     
-
+    
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBOutlet weak var searchCategoryTextField: UITextField!
+    
+    var searchText:String!
+    
+    
+    var task:Task!
+    
+    
+    
+    let realm = try! Realm()
+    //realmに保存するためにインスタンス化
+    
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+    // DB内のタスクが格納されるリスト。
+    // 日付の近い順でソート：昇順
+    // 以降内容をアップデートするとリスト内は自動的に更新される。
+    
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -23,12 +55,25 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        //taskの中にarrayの中に入っているデータを入れる
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        //formatterの型を指定
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
         
         return cell
     }
@@ -40,14 +85,85 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     
     
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        //セルが削除可能なことを知らせるメソッド
         return .delete
     }
     
-    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        <#code#>
+        //deleteボタンが押された時に呼ばれるメソッド
+        if editingStyle == .delete {
+            // 削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            
+            // ローカル通知をキャンセルする
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            
+            // データベースから削除する
+            try! realm.write {
+                self.realm.delete(task)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+                // .fadeで横スライドしながら消えていくアニメーションを表している。
+            }
+            
+            // 未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                //taskが管理している通知を出力させることでエラーが起きていないか確認する
+                for request in requests {
+                    print("/---------------")
+                    print(request)
+                    print("---------------/")
+                }
+            }
+        }
+        
+        
     }
-
-
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+                //idが被らないように全てのタスクのidの最大値からプラス１したものをidとする
+            }
+            
+            inputViewController.task = task
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    
+    @IBAction func search(_ sender: Any) {
+        
+        searchText = searchCategoryTextField.text!
+       
+        
+        taskArray = try! Realm().objects(Task.self).filter("category = '" + self.searchText + "'")
+    
+        tableView.reloadData()
+        
+      
+        
+        
+    }
+    
+    
+    
+    
 }
 
